de361902df969deb6ea1ce8ab7f86f7f6afd918a
fadhel <fadhel_chemmem@hotmail.com>
2023-02-04 01:25:14 +0100

    checkpoint

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 function articlePlus(e) {
-    e.target.parentNode.querySelector('#nbr').innerHTML++;
-    calculTotal(1, p = e.target.parentNode.querySelector('#prix').innerHTML);
+    e.target.closest('.article').querySelector('.nbr').innerHTML++;
+    calculTotal(1, p = e.target.closest('.article').querySelector('.prix').innerHTML);
 }
 function articleMinus(e) {
-    if (e.target.parentNode.querySelector('#nbr').innerHTML > 0) {
-        e.target.parentNode.querySelector('#nbr').innerHTML--;
-        calculTotal(-1, e.target.parentNode.querySelector('#prix').innerHTML);
+    if (e.target.closest('.article').querySelector('.nbr').innerHTML > 0) {
+        e.target.closest('.article').querySelector('.nbr').innerHTML--;
+        calculTotal(-1, e.target.closest('.article').querySelector('.prix').innerHTML);
     };
 }
 function calculTotal(n, p) {
     el = Number(document.getElementById('totalArticles').innerHTML);
     let totalModification = Number(el) + Number(p) * Number(n);
     document.getElementById('totalArticles').innerHTML = totalModification;
+}
+function articleDelete(e){
+    e.target.closest('.article').remove();
+}
+function modifierCouleur(e){
+    e.target.closest('.article').style.backgroundColor  = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
 }
